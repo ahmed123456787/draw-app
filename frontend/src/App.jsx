a3b5807b2af6d5404b0fea0d325d4ad7a5b1f126
ahmed123456787 <zaterahmed62@gmail.com>
2024-12-01 00:27:18 +0100

    resolve merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,8 +15,8 @@ const ColoredRect = () => {
     <Rect
       x={20}
       y={20}
-      width={50}
-      height={50}
+      width={500}
+      height={2000}
       fill={color}
       shadowBlur={5}
       onClick={handleClick}
