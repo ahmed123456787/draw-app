9fbe3baa987b66722c699a7e9245e252bae4adb1
ahmed123456787 <zaterahmed62@gmail.com>
2024-12-19 09:47:22 +0100

    adding the delete systen feature

--- backend/app/app/celery.py
+++ backend/app/app/celery.py
@@ -1,12 +1,17 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
-app = Celery("django_celery")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+app = Celery('app')
 
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f"Request: {self.request!r}")
+    print('Request: {0!r}'.format(self.request))
--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from celery.schedules import crontab
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,12 +162,15 @@ SPECTACULAR_SETTINGS = {
 
 # Celery settings
 CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
 
-#Celery Beat
 # CELERY BEAT SCHEDULER
-
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BEAT_SCHEDULE = {
+    "delete_old_draws": {
+        "task": "app.tasks.delete_old_draws",
+        "schedule": crontab(minute="*/1"),
+    },
+}
